e7ccc14f07c5e9ce5dc232969524363524601cef
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-17 00:44:13 -0700

    Use MapClient wrapper in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import dynamic from 'next/dynamic';
-
-// Dynamically import MapView with no SSR because it relies on window and navigator
-const MapView = dynamic(() => import('@/components/MapView'), { ssr: false });
+import MapClientComponent from '../components/MapClient';
 
 export default function Home() {
-  return <MapView />;
+  return <MapClientComponent />;
 }
