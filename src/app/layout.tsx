1883466932811504c13144a6dd862326c54dbd2f
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-18 04:05:28 -0700

    Apply forest colour palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-gray-50 text-gray-900">
+      <body className="min-h-screen flex flex-col">
         {children}
       </body>
     </html>
