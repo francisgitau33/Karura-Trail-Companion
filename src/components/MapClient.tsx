fc5dda3eee7194d59820c4346f39e01f9011a589
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-18 01:47:59 -0700

    Correct app imports and remove duplicate layout file

--- src/components/MapClient.tsx
+++ src/components/MapClient.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from "next/dynamic";
 
-// Dynamically import MapView with no SSR because it relies on browser APIs like `window` and `navigator`.
 const MapView = dynamic(() => import("./MapView"), {
   ssr: false,
 });
