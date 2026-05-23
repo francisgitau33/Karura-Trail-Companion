90f59755e458c4625b7e9f99d4e91b12f7e6df40
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-23 07:34:52 -0700

    Load approved place suggestions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import MapClientComponent from "../components/MapClient";
+import { getApprovedPlaceSuggestions } from "../lib/placeSuggestions";
 import { getSiteSettings } from "../lib/siteSettings";
 
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  const siteSettings = await getSiteSettings();
+  const [siteSettings, approvedPlaceSuggestions] = await Promise.all([
+    getSiteSettings(),
+    getApprovedPlaceSuggestions(),
+  ]);
 
-  return <MapClientComponent siteSettings={siteSettings} />;
+  return (
+    <MapClientComponent
+      siteSettings={siteSettings}
+      approvedPlaceSuggestions={approvedPlaceSuggestions}
+    />
+  );
 }
