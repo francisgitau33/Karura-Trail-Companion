9e1dd9c1221dc578db9cc92aca172b9f85c5aaa4
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-23 07:35:05 -0700

    Pass approved suggestions to map

--- src/components/MapClient.tsx
+++ src/components/MapClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import type { PublicPlaceSuggestion } from "../lib/placeSuggestions";
 import type { SiteSettings } from "../lib/siteSettings";
 import Footer from "./Footer";
 
@@ -8,10 +9,19 @@ const MapView = dynamic(() => import("./MapView"), {
   ssr: false,
 });
 
-export default function MapClientComponent({ siteSettings }: { siteSettings: SiteSettings }) {
+export default function MapClientComponent({
+  siteSettings,
+  approvedPlaceSuggestions,
+}: {
+  siteSettings: SiteSettings;
+  approvedPlaceSuggestions: PublicPlaceSuggestion[];
+}) {
   return (
     <>
-      <MapView siteSettings={siteSettings} />
+      <MapView
+        siteSettings={siteSettings}
+        approvedPlaceSuggestions={approvedPlaceSuggestions}
+      />
       <Footer contactEmail={siteSettings.contactEmail} />
     </>
   );
