62a6addbe8298e868d9f7e95bbd603be6949a5e9
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-23 21:49:44 -0700

    Add GPS trail contribution workflow

--- src/components/MapClient.tsx
+++ src/components/MapClient.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 import type { PublicPlaceSuggestion } from "../lib/placeSuggestions";
 import type { SiteSettings } from "../lib/siteSettings";
+import type { PublicTrailSuggestion } from "../lib/trailSuggestions";
 import Footer from "./Footer";
 
 const MapView = dynamic(() => import("./MapView"), {
@@ -12,15 +13,18 @@ const MapView = dynamic(() => import("./MapView"), {
 export default function MapClientComponent({
   siteSettings,
   approvedPlaceSuggestions,
+  approvedTrailSuggestions,
 }: {
   siteSettings: SiteSettings;
   approvedPlaceSuggestions: PublicPlaceSuggestion[];
+  approvedTrailSuggestions: PublicTrailSuggestion[];
 }) {
   return (
     <>
       <MapView
         siteSettings={siteSettings}
         approvedPlaceSuggestions={approvedPlaceSuggestions}
+        approvedTrailSuggestions={approvedTrailSuggestions}
       />
       <Footer contactEmail={siteSettings.contactEmail} />
     </>
