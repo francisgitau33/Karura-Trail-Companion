d0460ed8c5722c4fd5bba0f0ba454ccdd1385114
francisgitau33 <theforestforthetrees23@gmail.com>
2026-05-23 21:49:27 -0700

    Add GPS trail contribution workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import MapClientComponent from "../components/MapClient";
 import { getApprovedPlaceSuggestions } from "../lib/placeSuggestions";
 import { getSiteSettings } from "../lib/siteSettings";
+import { getApprovedTrailSuggestions } from "../lib/trailSuggestions";
 
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  const [siteSettings, approvedPlaceSuggestions] = await Promise.all([
-    getSiteSettings(),
+  const siteSettings = await getSiteSettings();
+  const [approvedPlaceSuggestions, approvedTrailSuggestions] = await Promise.all([
     getApprovedPlaceSuggestions(),
+    siteSettings.showApprovedTrails ? getApprovedTrailSuggestions() : Promise.resolve([]),
   ]);
 
   return (
     <MapClientComponent
       siteSettings={siteSettings}
       approvedPlaceSuggestions={approvedPlaceSuggestions}
+      approvedTrailSuggestions={approvedTrailSuggestions}
     />
   );
 }
